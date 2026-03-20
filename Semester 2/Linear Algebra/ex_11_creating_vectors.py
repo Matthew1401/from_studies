8c2bc55bcae515d720ef70564739ed0bac19f424
Mateusz Konieczny <119483964+Matthew1401@users.noreply.github.com>
2026-03-20 10:30:01 +0100

    Add script to create and display numpy vectors
    This script creates and displays vectors using numpy, including their sizes.

---
+++ Semester 2/Linear Algebra/ex_11_creating_vectors.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+a = np.arange(4, 35, 6)
+b = np.arange(-4, 2)
+c = np.linspace(-2, 1.5, 8)
+d = np.linspace(8, 4.5, 8)
+
+#Displaying the matrices
+print(f"The vector a = {a}")
+print(f"The vector b = {b}")
+print(f"The vector c = {c}")
+print(f"The vector d = {d}")
+
+#Brak in console
+print("\n")
+
+#Displaying the number of elements of each matrix
+print(f"a size = {a.size}")
+print(f"b size = {b.size}")
+print(f"c size = {c.size}")
+print(f"d size = {d.size}")
