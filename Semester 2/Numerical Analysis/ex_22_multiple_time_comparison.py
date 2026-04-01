f200f8c229a64d9d388ddb304dc5dbeb0993f6b5
Mateusz Konieczny <119483964+Matthew1401@users.noreply.github.com>
2026-04-01 10:27:50 +0200

    Add multiple time comparison for polynomial evaluation

---
+++ Semester 2/Numerical Analysis/ex_22_multiple_time_comparison.py
@@ -0,0 +1,33 @@
+import numpy as np
+from timeit import default_timer as tm
+
+coeff = [7, -4, 3, -12, 5]
+a = -5
+b = 5
+N = 100001
+arg = np.linspace(a, b, N)
+M = 100
+
+def poly1(x):
+    val = 0
+    for i in range(len(coeff)):
+        val += coeff[i] * x ** i
+    return val
+
+def poly2(x):
+    val = 0
+    for k in range(len(coeff) - 1, -1, -1):
+        val = coeff[k] + val * x
+    return val
+
+start_time1 = tm()
+for n in range(M):
+    values1 = poly1(arg)
+calc_time1 = tm() - start_time1
+
+start_time2 = tm()
+for n in range(M):
+    values2 = poly2(arg)
+calc_time2 = tm() - start_time2
+
+print(f"Time1 = {calc_time1}s\nTime2 = {calc_time2}s\nDifference of {calc_time1/calc_time2} times")
