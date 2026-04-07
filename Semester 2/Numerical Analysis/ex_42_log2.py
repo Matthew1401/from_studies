ed681ab822563dfde23ef85bf56c9805ccaf1b00
Mateusz Konieczny <119483964+Matthew1401@users.noreply.github.com>
2026-04-07 18:14:06 +0200

    Add log2 calculation and anharmonic series functions
    Implement functions to calculate anharmonic series and find indices for approximations of log(2).

---
+++ Semester 2/Numerical Analysis/ex_42_log2.py
@@ -0,0 +1,49 @@
+import numpy as np
+import math as m
+
+# Declare the constant functions d and epsilon
+d = 5
+eps = 1/(10 ** (d + 1))
+
+
+def anharmonic1(n):
+    """Function that calculates the sum of n-th value of a series"""
+    value = 0
+    terms = [value]
+    for k in range(1, n+1):
+        value += (-1)**(k + 1) / k
+        terms.append(value)
+    return terms
+
+
+def find_greatest_index(approx):
+    n = 1
+    terms = anharmonic1(int((1/approx)) + 1)
+    while True:
+        if abs(terms[n] - terms[n - 1]) < approx:
+            break
+        n += 1
+    return n
+
+
+def find_greatest_index_log(approx):
+    n = 1
+    terms = anharmonic1(int((1/approx)) + 1)
+    while True:
+        if abs(terms[n] - m.log(2)) < approx:
+            break
+        n += 1
+    return n
+
+# Calculate the values from required in the task
+n0 = find_greatest_index(eps)
+n1 = find_greatest_index_log(eps)
+
+a_n0 = anharmonic1(n0)[n0]
+a_n1 = anharmonic1(n1)[n1]
+
+print(f"n0: {n0}")
+print(f"anharmonic(n0): {round(a_n0, d+2)}")
+print(f"n1: {n1}")
+print(f"anharmonic(n1): {round(a_n1, d+2)}")
+print(f"log(2): {round(m.log(2), d+2)}")
